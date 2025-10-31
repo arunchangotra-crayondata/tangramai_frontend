f6eff59995609c1665f264991e4c00ecaef3dc8d
arunc@crayondata.com <arunc@crayondata.com>
2025-10-31 11:27:37 +0530

    fix temp

--- remove-credentials-filter.sh
+++ remove-credentials-filter.sh
@@ -10,8 +10,8 @@ import sys
 try:
     with open('$FILE', 'r', encoding='utf-8') as f:
         content = f.read()
-    content = content.replace('process.env.S3_ACCESS_KEY_ID || ""', 'process.env.S3_ACCESS_KEY_ID || \"\"')
-    content = content.replace('process.env.S3_SECRET_ACCESS_KEY || ""', 'process.env.S3_SECRET_ACCESS_KEY || \"\"')
+    content = content.replace('AKIA36OR25C2GKXHDJP4', 'process.env.S3_ACCESS_KEY_ID || \"\"')
+    content = content.replace('rPE7dcsPv63lRhTkUJwZ+7G7Gef43yu34ZEeydL', 'process.env.S3_SECRET_ACCESS_KEY || \"\"')
     with open('$FILE', 'w', encoding='utf-8') as f:
         f.write(content)
 except:
@@ -21,8 +21,8 @@ import sys
 try:
     with open('$FILE', 'r', encoding='utf-8') as f:
         content = f.read()
-    content = content.replace('process.env.S3_ACCESS_KEY_ID || ""', 'process.env.S3_ACCESS_KEY_ID || \"\"')
-    content = content.replace('process.env.S3_SECRET_ACCESS_KEY || ""', 'process.env.S3_SECRET_ACCESS_KEY || \"\"')
+    content = content.replace('AKIA36OR25C2GKXHDJP4', 'process.env.S3_ACCESS_KEY_ID || \"\"')
+    content = content.replace('rPE7dcsPv63lRhTkUJwZ+7G7Gef43yu34ZEeydL', 'process.env.S3_SECRET_ACCESS_KEY || \"\"')
     with open('$FILE', 'w', encoding='utf-8') as f:
         f.write(content)
 except:
